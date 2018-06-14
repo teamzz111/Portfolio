f4839712601d0e2ef22726b05ca8db826d7e836a
teamzz111 <aflr@outlook.com>
2018-06-14 17:04:42 -0500

    Skills zone finished

--- js/animaciones.js
+++ js/animaciones.js
@@ -3,7 +3,6 @@ $(document).ready( function(){
     var $window = $(window);
 
     function check_if_in_view() {
-        console.log('e');
         var window_height = $window.height();
         var window_top_position = $window.scrollTop();
         var window_bottom_position = (window_top_position + window_height);
@@ -18,10 +17,64 @@ $(document).ready( function(){
             if ((element_bottom_position >= window_top_position) &&
                 (element_top_position <= window_bottom_position)) {
                 $('#java').stop().animate({
-                    width: '100%'
+                    width: '62%'
                 },1500);
+                $('#pjava').text('62%');
+                $('#javafx').stop().animate({
+                    width: '43%'
+                }, 1500);
+                $('#pjavafx').text('43%');
+                $('#javaee').stop().animate({
+                    width: '30%'
+                }, 1500);
+                $('#pjavaee').text('30%');
+                $('#c').stop().animate({
+                    width: '60%'
+                }, 1500);
+                $('#pc').text('60%');
+                $('#kotlin').stop().animate({
+                    width: '10%'
+                }, 1500);
+                $('#pkotlin').text('10%');
+                $('#android').stop().animate({
+                    width: '20%'
+                }, 1500);
+                $('#pandroidstudio').text('20%');
+                $('#mysql').stop().animate({
+                    width: '52%'
+                }, 1500);
+                $('#pmysql').text('52%');
+                $('#html').stop().animate({
+                    width: '86%'
+                }, 1500);
+                $('#phtml').text('86%');
+                $('#css').stop().animate({
+                    width: '70%'
+                }, 1500);
+                $('#pcss').text('70%');
+                $('#javascript').stop().animate({
+                    width: '55%'
+                }, 1500);
+                $('#pjavascript').text('55%');
+                $('#jquery').stop().animate({
+                    width: '50%'
+                }, 1500);
+                $('#pjquery').text('50%');
+                $('#angular').stop().animate({
+                    width: '36%'
+                }, 1500);
+                $('#pangular').text('36%');
+                $('#php').stop().animate({
+                    width: '28%'
+                }, 1500);
+                $('#pphp').text('28%');
+                $('#typescript').stop().animate({
+                    width: '22%'
+                }, 1500);
+                $('#ptypescript').text('22%');
+
             } else {
-                $('#java').stop().animate({
+                $('#java, #javafx, #javaee, #c, #mysql, #android, #kotlin, #html, #css, #javascript, #jquery, #angular, #php, #typescript').stop().animate({
                     width: '0%'
                 }, 0);
             }
