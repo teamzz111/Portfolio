eb1e71c889488f2ffdf2be99e125e6cf37c3b2a7
Andres Largo <felipeflogxd@gmail.com>
2021-04-11 19:07:07 -0500

    Config seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: 'Andrés Largo',
+    description: 'I am a Systems engineering student focused on high level software development. I like to work on projects that have to do with mobile development, like Android apps and Web apps. The depth or difficulty is not a problem to me, I can solve it and make an amazing work for you. If you need to use some special language, I can make it happen.    ',
+    author: 'teamzz111',
   },
   plugins: [
     'gatsby-plugin-styled-components',
